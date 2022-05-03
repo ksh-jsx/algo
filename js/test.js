711c1f7e43ba2f32c0720d6df98cac04f25d6417
shkim787 <shkim061198@gmail.com>
2022-05-03 22:08:07 +0900

    프로그래머스 6

--- js/test.js
+++ js/test.js
@@ -1,9 +1,12 @@
-/*https://programmers.co.kr/learn/courses/30/lessons/12977*/
+/*https://programmers.co.kr/learn/courses/30/lessons/42748 */
+function solution(array, commands) {
+  var answer = []
+  for(let i=0;i<commands.length;i++){
+    let temp_arr = array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>{return a-b});
 
-function solution(nums) {
-  var answer = -1;
-  
+    answer.push(temp_arr[commands[i][2]]-1)
+  }
   return answer;
 }
 
-solution([1,2,3,4])
+solution([10, 50, 22, 63, 36, 79, 42]	, [[2, 5, 3], [4, 4, 1], [1, 7, 3]])
