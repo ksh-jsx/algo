fcd975574de1656f9e41b03638e2e3b39e5803f1
김성현 <shkim061198@gmail.com>
2023-03-31 13:39:50 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1383.68 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,12 @@
+function solution(elements) {
+    const set = new Set();
+    for (let i = 1; i <= elements.length; i++) {
+        
+        const els = elements.concat(elements.slice(0, i));
+        
+        for (let j = 0; j < elements.length; j++) {
+            set.add(els.slice(j, j + i).reduce((a, c) => a + c, 0));            
+        }
+    }
+    return set.size;
+}
