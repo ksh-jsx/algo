12270ec3c2396d7054080d35c366ff8fd5943ee6
김성현 <shkim061198@gmail.com>
2023-04-28 15:51:56 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 210.87 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -1,46 +1,4 @@
-// function solution(numbers) {
-//    const result = []
-   
-//    numbers.forEach((dec)=>{
-//        let target_dec = dec+1
-       
-//        while(true){    
-//            if(countDifferentBits(dec,target_dec)<3) break
-           
-//            target_dec++
-//        }
-       
-//        result.push(target_dec)
-//    })
-    
-//    return result
-// }
-
-// function countDifferentBits(a, b) {
-//   let count = 0
-//   const binaryA = ('0'+parseInt(a).toString(2)).split("")
-//   const binaryB = parseInt(b).toString(2).split("")
-  
-//   if(binaryB.length < binaryA.length) binaryB.unshift('0')
-    
-//   const maxLength = Math.max(binaryA.length, binaryB.length)
-
-//   for (let i = 0; i < maxLength; i++) {
-//     if (binaryA[i] !== binaryB[i]) {
-//       count++
-//     }
-//   }
-
-//   return count
-// }
-
 function solution(numbers) {
-    const tmp = numbers.map((v,i) => {    
-        let toStr = v.toString(2).split('').reverse().indexOf('0')
-
-        if(toStr === -1) return v + 2 ** (v.toString(2).length - 1)
-        return v + Math.ceil(2 ** (toStr -1))
-    });
-
-    return tmp
+    var answer = [];
+    return answer;
 }
