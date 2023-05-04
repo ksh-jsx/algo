673bfe6d680bcdedcc80ac75cbe5e229a348b19d
김성현 <shkim061198@gmail.com>
2023-05-04 14:46:42 +0900

    [level 2] Title: 괄호 변환, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
@@ -1,48 +1,10 @@
 function solution(p) {
-    var answer = '';
-    var open = 0;
-    var close = 0;
-    
-    // 1. 재귀 탈출 조건
-    if (!p) return ''; 
-    
-    for (let i=0; i<p.length; i++) {
-        if (p[i] === '(') open++
-        else close++
+    let count = 0
+    for(let i=0; i<p.length;i++){
+        const target = p[i]
+        if(target === '(') count++
+        else if(target === ')') count--
         
-        // 균형일 때 "올바른 괄호 문자열"인지 아닌지 판단
-        if (open === close) {
-            // 2, 3, 3-1
-            if (check(p.slice(0, i+1))) {
-                answer = p.slice(0, i+1) + solution(p.slice(i+1));
-                return answer;
-            } else {
-                // 4-1, 4-2, 4-3
-                answer = '(' + solution(p.slice(i+1)) + ')';
-                
-                // 4-4
-                for (let j=1; j<i; j++) {
-                    if (p[j] === '(') {
-                        answer = answer + ')'
-                    } else {
-                        answer = answer + '('
-                    }
-                }
-                // 4-5
-                return answer;
-            }
-        }
-    }    
-}
-
-function check(p) {
-    var stack = [];
-    for (let i = 0; i<p.length; i++) {
-        if (p[i] === '(') stack.push('(')
-        else {
-            if (stack.length === 0) return false;
-            stack.pop();
-        }
+        if(count === 0 ) console.log('균형')
     }
-    return true;
 }
