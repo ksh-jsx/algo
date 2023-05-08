e3e77576397570e7157b7b4ee375bdd1aeae12d6
김성현 <shkim061198@gmail.com>
2023-05-08 16:53:53 +0900

    [level 2] Title: 택배상자, Time: 194.39 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.js
@@ -0,0 +1,22 @@
+function solution(order) {
+  const stack = [];
+  let answer = 0;
+  let boxNum = 1;
+    
+  for (const idx of order) {
+    let checker = false;
+      
+    while (true) {
+      if (stack.length === 0) stack.push(boxNum++);
+      if (idx > stack.at(-1)) stack.push(boxNum++);
+      else if (idx === stack.at(-1)) {
+        stack.pop();
+        answer++;
+        checker = true;
+        break;
+      } else break;
+    }
+    if (!checker) break;
+  }
+  return answer;
+}
