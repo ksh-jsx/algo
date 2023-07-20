644192127c407830e91f7edc08612cbb84b081c0
김성현 <shkim061198@gmail.com>
2023-07-20 13:54:14 +0900

    [unrated] Title: 특수문자 출력하기, Time: 22.27 ms, Memory: 16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string specialCharacters = "!@#$%^&*(\\'\"<>?:;";
+
+        Console.WriteLine(specialCharacters);
+    }
+}
